ffdb8bf4332c839736e5cccc19075f6c1c2ec27e
oussamabonnor1 <oussamabonnor@gmail.com>
2018-11-30 18:03:31 +0100

    adding activity first round logic & models

---
+++ app/src/main/java/com/jetlight/uprice/Models/Product.java
@@ -0,0 +1,39 @@
+package com.jetlight.uprice.Models;
+
+import android.net.Uri;
+
+public class Product {
+    private String name;
+    private int price;
+    private int imageLink;
+
+    public Product(String name, int price, int imageLink) {
+        this.name = name;
+        this.price = price;
+        this.imageLink = imageLink;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(int imageLink) {
+        this.imageLink = imageLink;
+    }
+}
--- app/src/main/java/com/jetlight/uprice/PlayersActivity.java
+++ app/src/main/java/com/jetlight/uprice/PlayersActivity.java
@@ -1,12 +1,16 @@
 package com.jetlight.uprice;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class PlayersActivity extends AppCompatActivity {
+import com.jetlight.uprice.Models.Product;
 
+import java.util.ArrayList;
+
+public class PlayersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
