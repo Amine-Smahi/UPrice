faaf76285874c64134fe827401b4a823fed2bf88
Amine Smahi <mohammed.amine.smahi@gmail.com>
2018-11-30 15:25:17 +0100

    Add initial project structure

---
+++ app/src/main/java/com/jetlight/uprice/PhaseTwoActivity.java
@@ -0,0 +1,13 @@
+package com.jetlight.uprice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhaseTwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phase_two);
+    }
+}
