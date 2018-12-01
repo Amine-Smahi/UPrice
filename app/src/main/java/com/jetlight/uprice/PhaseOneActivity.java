370275d02cbe4b91846193c0f091284425b8de46
Amine Smahi <mohammed.amine.smahi@gmail.com>
2018-12-01 15:56:52 +0100

    Add countdown timer

--- app/src/main/java/com/jetlight/uprice/PhaseOneActivity.java
+++ app/src/main/java/com/jetlight/uprice/PhaseOneActivity.java
@@ -1,6 +1,8 @@
 package com.jetlight.uprice;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,8 @@ import com.jetlight.uprice.Models.Product;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PhaseOneActivity extends AppCompatActivity {
 
@@ -21,9 +25,9 @@ public class PhaseOneActivity extends AppCompatActivity {
     TextView textView;
     int productPrice;
     EditText editText;
+    TextView clock;
     int [] playersPrices;
     static int j=0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +35,31 @@ public class PhaseOneActivity extends AppCompatActivity {
         imageView = (ImageView)findViewById(R.id.ImageViewproductImage);
         textView = (TextView)findViewById(R.id.TextViewproductName);
         editText = (EditText)findViewById(R.id.EditTextproductPrice);
+        clock = (TextView) findViewById(R.id.clock);
         Intent intent = getIntent();
         products.add(new Product("OMO 250 KG",65, R.mipmap.omo));
         products.add(new Product("ARIEL 250 KG",44,R.mipmap.ariel));
         playersPrices = new int[intent.getIntExtra("playersCount",0)];
         settingInfo();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new CountDownTimer(15000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                if ((millisUntilFinished/1000)  >= 10) {
+                    clock.setText("00:" + (millisUntilFinished/1000));
+                } else {
+                    clock.setText("00:0" + (millisUntilFinished/1000));
+                }
+            }
+
+            public void onFinish() {
+             //   mTextField.setText("done!");
+            }
+        }.start();
     }
 
     void settingInfo(){
@@ -69,5 +93,10 @@ public class PhaseOneActivity extends AppCompatActivity {
 
     private void winner() {
         settingInfo();
+    }
+
+    public void Restart(View view) {
+        Intent intent = new Intent(this,PlayersActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/jetlight/uprice/PhaseTwoActivity.java
+++ app/src/main/java/com/jetlight/uprice/PhaseTwoActivity.java
@@ -1,13 +1,35 @@
 package com.jetlight.uprice;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PhaseTwoActivity extends AppCompatActivity {
 
+    TextView clock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phase_two);
+        clock = (TextView) findViewById(R.id.clock);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new CountDownTimer(15000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                if ((millisUntilFinished/1000)  >= 10) {
+                    clock.setText("00:" + (millisUntilFinished/1000));
+                } else {
+                    clock.setText("00:0" + (millisUntilFinished/1000));
+                }
+            }
+
+            public void onFinish() {
+                //   mTextField.setText("done!");
+            }
+        }.start();
     }
 }
