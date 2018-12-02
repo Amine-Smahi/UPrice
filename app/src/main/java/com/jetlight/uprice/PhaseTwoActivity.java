e219de864ca7e7724201009a5edaea945fc3b7c8
Amine Smahi <mohammed.amine.smahi@gmail.com>
2018-12-02 10:18:56 +0100

    Add event on click for buttons

--- app/src/main/java/com/jetlight/uprice/PhaseTwoActivity.java
+++ app/src/main/java/com/jetlight/uprice/PhaseTwoActivity.java
@@ -3,16 +3,20 @@ package com.jetlight.uprice;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PhaseTwoActivity extends AppCompatActivity {
 
     TextView clock;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phase_two);
         clock = (TextView) findViewById(R.id.clock);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView.setImageDrawable(getResources().getDrawable(R.mipmap.omo));
     }
     @Override
     protected void onStart() {
