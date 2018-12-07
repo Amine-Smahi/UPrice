db7f842b74a3d7d4658d07147c86748c96068030
Mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2018-12-07 18:15:26 +0100

    settingInfo work in progress

--- app/src/main/java/com/jetlight/uprice/PhaseTwoActivity.java
+++ app/src/main/java/com/jetlight/uprice/PhaseTwoActivity.java
@@ -8,8 +8,15 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jetlight.uprice.Models.Product;
+
+import java.util.Random;
+
+import static com.jetlight.uprice.Models.ProductsSeed.products;
+
 public class PhaseTwoActivity extends AppCompatActivity {
 
+    Product product;
     TextView clock;
     ImageView imageView;
     @Override
@@ -18,8 +25,33 @@ public class PhaseTwoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_phase_two);
         clock = (TextView) findViewById(R.id.clock);
         imageView = (ImageView) findViewById(R.id.imageView2);
-        imageView.setImageDrawable(getResources().getDrawable(R.mipmap.omo));
+        Intent intent = getIntent();
+
     }
+   /* void settingInfo(){
+        if(products.size()!=0) {
+            int randomIndex = new Random().nextInt(products.size());
+            product = products.get(randomIndex);
+            imageView.setImageDrawable(getResources().getDrawable(product.getImageLink()));
+            textView.setText(product.getName());
+            productPrice = product.getPrice();
+            playerSelection();
+            products.remove(randomIndex);
+        }
+        else {
+            winnersPhaseOne();
+            ViewDialog.showDialog(PhaseOneActivity.this, "  Congrats \n Player" + winner1 + " and Player" + winner2 + " won!  ", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(PhaseOneActivity.this,PhaseTwoActivity.class);
+                    intent.putExtra("winner1",winner1);
+                    intent.putExtra("winner2",winner2);
+                    startActivity(intent);
+                }
+            });
+
+        }
+    }*/
     public void Restart(View view) {
         Intent intent = new Intent(this,PlayersActivity.class);
         startActivity(intent);
